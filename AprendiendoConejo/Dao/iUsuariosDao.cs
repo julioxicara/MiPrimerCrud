3de95dfa2cc7c7fd99170380d991ba94e436e766
julioxicara <ranaxtg@gmail.com>
2017-06-13 20:10:33 -0600

    mi primer crud

---
+++ AprendiendoConejo/Dao/iUsuariosDao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AprendiendoConejo.Models;
+
+namespace AprendiendoConejo.Dao
+{
+    interface iUsuariosDao
+    {
+        string crear(usuario varusuario);
+        string modificar(usuario varusuario);
+        string eliminar(string correcoelectronico);
+        usuario buscarId(String correoelectronico);
+        String Login(String correoelectronico, String pass);
+        List<usuario> listar();
+
+    }
+}
---
+++ AprendiendoConejo/Dao/usuariosDao.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AprendiendoConejo.Models;
+using System.Web;
+
+namespace AprendiendoConejo.Dao
+{
+    public class usuariosDao:iUsuariosDao
+    {
+        private usuariosEntities conexion = new usuariosEntities();        
+
+        public string crear(usuario varusuario)
+        {
+            try
+            {
+                conexion.usuario.Add(varusuario);
+                conexion.SaveChanges();
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                return "Ha ocurrido un error " + ex.Message;
+            }            
+        }
+
+        public string modificar(usuario varusuario)
+        {
+            try
+            {
+                conexion.Entry(varusuario).State = System.Data.EntityState.Modified;
+                conexion.SaveChanges();
+                return "OK";
+            }
+
+            catch (Exception ex)
+            {
+                return "Ha ocurrido un error " + ex.Message;
+            }            
+        }
+
+        public string eliminar(string correoelectronico)
+        {
+            usuario user = new usuario();
+
+            try
+            {
+                user = conexion.usuario.Find(correoelectronico);
+                conexion.usuario.Remove(user);
+                conexion.SaveChanges();
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                return "Ha ocurrido un error " + ex.Message;
+            }            
+        }
+
+        public usuario buscarId(string correoelectronico)
+        {
+            usuario user = new usuario();
+
+            try
+            {
+                user = conexion.usuario.Find(correoelectronico);
+                return user;
+            }
+
+            catch
+            {
+                return user;
+            }  
+        }
+
+        public string Login(string correoelectronico, string pass)
+        {
+            usuario user = new usuario();
+
+            try
+            {
+                user = conexion.usuario.Find(correoelectronico);
+                if (user.correoelectronico == correoelectronico && user.pass == pass)
+                {
+                    return "OK";
+                }
+
+                else
+                {
+                    return "Sus credenciales no son validas";
+                }
+
+                
+            }
+            catch (Exception ex)
+            {
+                return "Ha ocurrido un error " + ex.Message;
+            }            
+        }
+
+        public List<usuario> listar()
+
+        {
+            List<usuario> lista = new List<usuario>();
+            lista = conexion.usuario.ToList();
+            return lista;
+        }
+    }
+}
